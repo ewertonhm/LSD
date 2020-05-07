b6f0eb03c8f5e667c782959d45adb61bbc86f097
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2020-05-07 20:43:46 -0300

    upload exemplo

---
+++ old php/upararquivo.php
@@ -0,0 +1,9 @@
+<?php
+  require_once "Classes/db/DBConexao.php";
+ ?>
+<form enctype="multipart/form-data" action="recebido.php" method="POST">
+    <!-- O Nome do elemento input determina o nome da array $_FILES -->
+    Enviar esse arquivo: <input name="arquivo" type="file" />
+    <br>
+    <input type="submit" value="Enviar arquivo" />
+</form>
